47d9d04309bfd062eea676ccfab8656e9cfbff82
unknown <berkozzambak@gmail.com>
2021-12-27 17:00:24 +0100

    Nested function structure

--- Refactoring (Fowler)/Chapter1/index.js
+++ Refactoring (Fowler)/Chapter1/index.js
@@ -13,63 +13,63 @@ export const statement = (invoice, plays) => {
   result += `Amount owed is ${usd(totalAmount(invoice))}`;
   result += `You earned ${totalVolumeCredits(invoice)} credits`;
   return result;
-};
 
-const amountFor = (aPerformance) => {
-  let result = 0;
-  switch (playFor(aPerformance).type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30)
-        result += 1000 * (aPerformance.audience - 30);
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20)
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`Unknown play type: ${playFor(aPerformance).type}`);
+  function amountFor(aPerformance) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30)
+          result += 1000 * (aPerformance.audience - 30);
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20)
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`Unknown play type: ${playFor(aPerformance).type}`);
+    }
+    return result;
   }
-  return result;
-};
 
-const playFor = (aPerformance) => {
-  return plays[aPerformance.playID];
-};
-
-const totalAmount = (invoice) => {
-  let result = 0;
-  for (let perf of invoice.performances) {
-    result += amountFor(perf);
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
-  return result;
-};
 
-const totalVolumeCredits = (invoice) => {
-  let result = 0;
-  for (let perf of invoice.performances) {
-    result += volumeCreditsFor(perf);
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
   }
-  return result;
-};
 
-const volumeCreditsFor = (aPerformance) => {
-  let result = 0;
-  result += Math.max(aPerformance.audience - 30, 0);
-  //Add extra creadt for every ten comedy attendees
-  if ("comedy" == playFor(aPerformance).type)
-    result += Math.floor(aPerformance.audience / 5);
-  return result;
-};
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += volumeCreditsFor(perf);
+    }
+    return result;
+  }
 
-const usd = (aNumber) => {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format(aNumber / 100);
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    //Add extra creadt for every ten comedy attendees
+    if ("comedy" == playFor(aPerformance).type)
+      result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
+  }
 };
 
 for (let invoice of invoices) {
