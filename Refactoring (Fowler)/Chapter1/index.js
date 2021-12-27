80306a8153e134b8b34aeb5292307bacca9ac481
unknown <berkozzambak@gmail.com>
2021-12-27 16:13:15 +0100

    Replace Temp with Query && Inline Variable (variable named play)

--- Refactoring (Fowler)/Chapter1/index.js
+++ Refactoring (Fowler)/Chapter1/index.js
@@ -12,16 +12,16 @@ export const statement = (invoice, plays) => {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = amountFor(perf, play);
+    let thisAmount = amountFor(perf, playFor(perf));
 
     // Add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     //Add extra creadt for every ten comedy attendees
-    if ("comedy" == play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" == playFor(perf))
+      volumeCredits += Math.floor(perf.audience / 5);
 
     //print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf.audience
     } seats)\n`;
     totalAmount += thisAmount;
@@ -49,6 +49,10 @@ const amountFor = (aPerformance, play) => {
       throw new Error(`Unknown play type: ${play.type}`);
   }
   return result;
+};
+
+const playFor = (aPerformance) => {
+  return plays[aPerformance.playID];
 };
 
 for (let invoice of invoices) {
