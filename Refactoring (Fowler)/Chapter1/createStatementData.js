111fd11e76627c1769e83c1681df7b7d4b2630ea
unknown <berkozzambak@gmail.com>
2021-12-27 18:14:59 +0100

    Move volumeCreditsFor logic into calculator class and adjust names

--- Refactoring (Fowler)/Chapter1/createStatementData.js
+++ Refactoring (Fowler)/Chapter1/createStatementData.js
@@ -23,6 +23,14 @@ class PerformanceCalculator {
     }
     return result;
   }
+  get volumeCredits() {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+    //Add extra creadt for every ten comedy attendees
+    if ("comedy" == this.play.type)
+      result += Math.floor(this.performance.audience / 5);
+    return result;
+  }
 }
 
 function createStatementData(invoice, plays) {
@@ -41,7 +49,7 @@ function createStatementData(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
     result.amount = calculator.amount;
-    result.volumeCredits = volumeCreditsFor(result);
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
   function playFor(aPerformance) {
@@ -52,12 +60,8 @@ function createStatementData(invoice, plays) {
       .amount;
   }
   function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    //Add extra creadt for every ten comedy attendees
-    if ("comedy" == aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance))
+      .volumeCredits;
   }
   function totalAmount(data) {
     return data.performances.reduce((total, p) => total + p.amount, 0);
